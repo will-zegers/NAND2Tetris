5eadf10e5eedae339046b7553ff629d5a3099c03
Will Zegers <will.zegers@gmail.com>
2026-05-26 23:10:32 -0700

    First commit

---
+++ project4/Mult.asm
@@ -0,0 +1,31 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/4/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// The algorithm is based on repetitive addition.
+
+@0
+D=A
+@R2
+M=D
+
+(LOOP)
+  @R1
+  MD=M-1
+  @END
+  D;JLT
+
+  @R0
+  D=M
+  @R2
+  M=D+M
+
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
